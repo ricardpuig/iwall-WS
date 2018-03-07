de68c21865e939b7758f0b77273d46aacbdd0115
Ricard Puig <ricardpuig@Ricards-MacBook-Pro.local>
2018-03-07 14:43:43 +0100

    first version

---
+++ index.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var osc = require("osc");
+var WebSocket = require("ws");
+
+var messages = [{
+  id: 1,
+  text: "Hola soy un mensaje",
+  author: "Carlos Azaustre"
+}];
+
+
+var getIPAddresses = function () {
+    var os = require("os"),
+        interfaces = os.networkInterfaces(),
+        ipAddresses = [];
+
+    for (var deviceName in interfaces) {
+        var addresses = interfaces[deviceName];
+        for (var i = 0; i < addresses.length; i++) {
+            var addressInfo = addresses[i];
+            if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+                ipAddresses.push(addressInfo.address);
+            }
+        }
+    }
+
+    return ipAddresses;
+};
+
+// Bind to a UDP socket to listen for incoming OSC events.
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 57121
+});
+
+udpPort.on("ready", function () {
+    var ipAddresses = getIPAddresses();
+    console.log("Listening for OSC over UDP.");
+    ipAddresses.forEach(function (address) {
+        console.log(" Host:", address + ", Port:", udpPort.options.localPort);
+    });
+    console.log("To start the demo, go to http://localhost:8081 in your web browser.");
+});
+
+udpPort.open();
+
+// Create an Express-based Web Socket server to which OSC messages will be relayed.
+var appResources = __dirname + "/web",
+    app = express(),
+    server = app.listen(8081),
+    wss = new WebSocket.Server({
+        server: server
+    });
+
+app.use("/", express.static(appResources));
+wss.on("connection", function (socket) {
+    console.log("A Web Socket connection has been established!");
+    var socketPort = new osc.WebSocketPort({
+        socket: socket
+    });
+
+    var relay = new osc.Relay(udpPort, socketPort, {
+        raw: true
+    });
+});
+
+
+
+app.use(express.static('public'));
+
+app.get('/hello', function(req, res) {
+  res.status(200).send("Hello World!");
+});
+
+io.on('connection', function(socket) {
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
+
+  socket.on('new-message', function(data) {
+    messages.push(data);
+    console.log("Recibido ", data);
+
+    io.sockets.emit('messages', messages);
+  });
+});
+
+server.listen(8080, function() {
+  console.log("Servidor corriendo en http://localhost:8080");
+});
---
+++ public/js/main2.js
@@ -0,0 +1,92 @@
+var messageDiv;
+var statusDiv;
+var button;
+var button2;
+var button3;
+var textField;
+var socket = io.connect('http://localhost:8080', { 'forceNew': true });
+
+//escuchar evento messages
+socket.on('messages', function(data) {
+  console.log(data);
+  render(data);
+})
+
+
+//recibir mensajes
+function render (data) {
+  var html = data.map(function(elem, index) {
+    return(`<div>
+              <strong>${elem.author}</strong>:
+              <em>${elem.text}</em>
+            </div>`);
+  }).join(" ");
+
+  document.getElementById('messages').innerHTML = html;
+}
+
+//emitir mensajes
+function addMessage1(e) {
+
+
+	message = document.getElementById("message");
+	message.value = "1";
+	socket.emit('new-message', message.value);
+  return false;
+}
+
+//emitir mensajes
+function addMessage3(e) {
+
+
+	message = document.getElementById("message");
+	message.value = "2";
+	socket.emit('new-message', message.value);
+  return false;
+}
+
+/*
+
+// send value from text input
+function sendMessageForm(){
+	socket.send(message.value);
+	message.value = "";
+}
+
+// setup web socket
+
+
+/*
+function setupSocket(){
+
+	// setup websocket
+	// get_appropriate_ws_url is a nifty function by the libwebsockets people
+	// it decides what the websocket url is based on the broswer url
+	// e.g. https://mygreathost:9099 = wss://mygreathost:9099
+
+
+	// setup!
+	socket = new WebSocket( get_appropriate_ws_url());
+
+	// open
+	try {
+		socket.onopen = function() {
+			statusDiv.style.backgroundColor = "#40dd40";
+			statusDiv.textContent = " Connected";
+		}
+
+		// received message
+		socket.onmessage =function got_packet(msg) {
+			messageDiv.innerHTML = msg.data + "<br />" + messageDiv.innerHTML;
+		}
+
+		socket.onclose = function(){
+			statusDiv.style.backgroundColor = "#ff4040";
+			statusDiv.textContent = " Closed ";
+		}
+	} catch(exception) {
+		alert('<p>Error' + exception);
+	}
+}
+
+*/
